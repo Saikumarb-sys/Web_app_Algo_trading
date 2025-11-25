87ee05f6487e3585bfd7c966ea0abc97be361610
Saikumarb-sys <boddub.saikumar@gmail.com>
2025-11-25 16:42:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-API_KEY = "4uvlgrkxqg7ml4ws"
-API_SECRET = "trof1jo1i35kmbndtdizg7xd98ixuzf5"
+import streamlit as st
+
+API_KEY = st.secrets["4uvlgrkxqg7ml4ws"]
+API_SECRET = st.secrets["trof1jo1i35kmbndtdizg7xd98ixuzf5"]
